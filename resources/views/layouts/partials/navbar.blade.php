fe8943eec3563bbcd806cf9bc9b6ea9e4090e460
braianlm <bleonelm@gmail.com>
2019-07-16 19:23:45 -0300

    css buscador

--- resources/views/layouts/partials/navbar.blade.php
+++ resources/views/layouts/partials/navbar.blade.php
@@ -7,31 +7,32 @@
     </button>
 
     <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav cascadaUno">      
-            <li class="nav-item dropdown">
+        <ul class="navbar-nav cascadaUno" style="justify-content:flex-end; align-items:center">      
+            <li class="nav-item dropdown d-flex" >
                 <a class="nav-link usuario dropdown-toggle" data-toggle="dropdown" href="/profile">{{Auth::user()->name}}</a>
                 <ul class="dropdown-menu cascadaDos">
+
                     <li class="borderli">
                         <a href="/profile">Perfil</a>
                     </li>
                     <li>
-                        <a href="#">Editar Perfil</a>
-                    </li>
-                    <li class="borderli">
-                        <a href="#">Configuración</a>
-                    </li>
-                    <li>
                         <a href="/logout">Cerrar Sesión</a>
                     </li>
+
                 </ul>
             </li>
+
             <li class="nav-item">
                 <a class="nav-link" href="/faq">FAQ</a>
             </li>
-            <form action="/productos/busqueda" class="form" method="GET">
-                        <input type="text" name="clave">
-                        <input type="submit" class="btn btn-info" name="" id="" style="background-color:yellow; color:black; font-weight:bold; border: 1px solid yellow; margin:5%auto;">
-            </form>
+
+            <li class="nav-item" >
+                <form action="/productos/busqueda" class="form" method="GET" style="display:flex; flex-direction:row">
+                            <input type="text" name="clave" class="input-group-text mb-3 mt-3 mr-3">
+                            <input type="submit" class="btn btn-info" name="" id="" style="background-color:yellow; color:black; font-weight:bold; border: 1px solid yellow; margin:5%auto;">
+                </form>
+            </li>
+
         </ul>
     </div>
 </nav>
